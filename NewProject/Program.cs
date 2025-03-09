5f4cb9dc8a7080a0d79ae0aca78aba8398a516ef
sufyan2618 <155754649+sufyan2618@users.noreply.github.com>
2025-03-09 20:21:14 +0500

    Add project files.

---
+++ NewProject/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class Project
+{
+    public static void Main()
+    {
+        Console.WriteLine("Please Enter a number: ");
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine("You entered {0}", n);
+
+    }
+}
